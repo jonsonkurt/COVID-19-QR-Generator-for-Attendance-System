5492dd6b6d1a0e5d318bdfec7108392ae2c97c04
_jonsonkurt <jonsonkurt@gmail.com>
2021-12-05 21:52:46 +0800

    Updated generate function

--- libs/baseclass/generate.py
+++ libs/baseclass/generate.py
@@ -1,11 +1,25 @@
-from libs.baseclass import information
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen
+from kivymd.toast.kivytoast import toast
+import sqlite3
 
 Builder.load_file('./libs/kv/generate.kv')
 
 class GenerateScreen(Screen):
+
+    def check_information(self):
+
+        conn = sqlite3.connect("mybase.db")
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS information(id_num integer PRIMARY KEY, full_name VARCHAR(30), course_section VARCHAR(30), student_number VARCHAR(30), contact_number VARCHAR(30))")
+        cur.execute("SELECT id_num FROM information")
+        results = cur.fetchall()
+        if len(results) == 0:
+            conn.close()
+            return toast('Please update your information.')
+        else:
+            self.manager.current = "health_declaration"
 
     def generate(self):
 
