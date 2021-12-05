a5333b40e52f19a2014dbfdfeea401a28c1bb7fe
_jonsonkurt <jonsonkurt@gmail.com>
2021-12-05 21:52:07 +0800

    Deleted update file

--- libs/baseclass/update.py
+++
@@ -1,8 +0,0 @@
-from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import Screen
-from kivymd.toast.kivytoast import toast
-
-Builder.load_file('./libs/kv/update.kv')
-
-class UpdateScreen(Screen):
-    pass
